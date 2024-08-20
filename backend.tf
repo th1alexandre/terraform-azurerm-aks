0bc94c19ad3eb53e91aa6eb13c18c5e0f0a7e15c
th1alexandre <github@th1alexandre.dev>
2024-08-19 21:25:35 -0400

    initialize azurerm backend

--- backend.tf
+++ backend.tf
@@ -18,3 +18,12 @@ module "backend" {
     managed_by = "terraform"
   }
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"         # tf_backend_resource_group
+    storage_account_name = "t1atfazaksbackend"  # tf_backend_storage_account
+    container_name       = "terraform-state"    # tf_backend_container
+    key                  = "terraform.tfstate"  # tf_backend_state_file_key
+  }
+}
