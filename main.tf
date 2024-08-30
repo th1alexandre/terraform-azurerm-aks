12ce7687be73b7f6a722fc7f43042a6dcc15915e
th1alexandre <github@th1alexandre.dev>
2024-08-30 00:22:22 -0400

    create virtual network

--- main.tf
+++ main.tf
@@ -6,3 +6,22 @@ module "resource_group" {
 
   tags = var.tags
 }
+
+module "virtual_network" {
+  source = "./modules/azurerm_virtual_network"
+
+  vnet_name           = var.vnet_name
+  vnet_address_space  = var.vnet_address_space
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
+
+  subnet1_name           = var.subnet1_name
+  subnet1_address_prefix = var.subnet1_address_prefix
+  # subnet1_nsg_security_rules = [{ # Optional }]
+
+  subnet2_name           = var.subnet2_name
+  subnet2_address_prefix = var.subnet2_address_prefix
+  # subnet2_nsg_security_rules = [{ # Optional }]
+
+  tags = var.tags
+}
--- variables.tf
+++ variables.tf
@@ -9,6 +9,37 @@ variable "resource_group_location" {
   type        = string
 }
 
+# Virtual Network
+variable "vnet_name" {
+  description = "The name of the virtual network"
+  type        = string
+}
+
+variable "vnet_address_space" {
+  description = "The address space for the virtual network"
+  type        = string
+}
+
+variable "subnet1_name" {
+  description = "The name of the first subnet"
+  type        = string
+}
+
+variable "subnet1_address_prefix" {
+  description = "The address prefix for the first subnet"
+  type        = string
+}
+
+variable "subnet2_name" {
+  description = "The name of the second subnet"
+  type        = string
+}
+
+variable "subnet2_address_prefix" {
+  description = "The address prefix for the second subnet"
+  type        = string
+}
+
 # All resources
 variable "tags" {
   description = "A map of tags to add to all resources"
