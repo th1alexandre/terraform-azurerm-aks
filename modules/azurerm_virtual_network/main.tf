1ddf7bf98ffa820e6f6ef612024de572ca878dc6
th1alexandre <github@th1alexandre.dev>
2024-08-29 22:45:58 -0400

    add subnet security group

--- modules/azurerm_virtual_network/main.tf
+++ modules/azurerm_virtual_network/main.tf
@@ -23,10 +23,82 @@ resource "azurerm_subnet" "subnet1" {
   address_prefixes = [var.subnet1_address_prefix]
 }
 
+resource "azurerm_network_security_group" "subnet1_nsg" {
+  name                = "${var.subnet1_name}_nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  dynamic "security_rule" {
+    for_each = var.subnet1_nsg_security_rules != null ? var.subnet1_nsg_security_rules : []
+    content {
+      name                       = security_rule.value.name
+      protocol                   = security_rule.value.protocol
+      source_port_range          = security_rule.value.source_port_range
+      destination_port_range     = security_rule.value.destination_port_range
+      source_address_prefixes    = security_rule.value.source_address_prefixes
+      destination_address_prefix = security_rule.value.destination_address_prefixes
+      access                     = security_rule.value.access
+      priority                   = security_rule.value.priority
+      direction                  = security_rule.value.direction
+
+      # description                                = "*"
+      # source_port_ranges                         = ["*"]
+      # destination_port_ranges                    = ["*"]
+      # source_address_prefix                      = "*"
+      # source_application_security_group_ids      = ["*"]
+      # destination_address_prefixes               = ["*"]
+      # destination_application_security_group_ids = ["*"]
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet1" {
+  subnet_id                 = azurerm_subnet.subnet1.id
+  network_security_group_id = azurerm_network_security_group.subnet1_nsg.id
+}
+
 resource "azurerm_subnet" "subnet2" {
   name                 = var.subnet2_name
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.this.name
 
   address_prefixes = [var.subnet2_address_prefix]
+}
+
+resource "azurerm_network_security_group" "subnet2_nsg" {
+  name                = "${var.subnet2_name}_nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  dynamic "security_rule" {
+    for_each = var.subnet2_nsg_security_rules != null ? var.subnet2_nsg_security_rules : []
+    content {
+      name                       = security_rule.value.name
+      protocol                   = security_rule.value.protocol
+      source_port_range          = security_rule.value.source_port_range
+      destination_port_range     = security_rule.value.destination_port_range
+      source_address_prefixes    = security_rule.value.source_address_prefixes
+      destination_address_prefix = security_rule.value.destination_address_prefixes
+      access                     = security_rule.value.access
+      priority                   = security_rule.value.priority
+      direction                  = security_rule.value.direction
+
+      # description                                = "*"
+      # source_port_ranges                         = ["*"]
+      # destination_port_ranges                    = ["*"]
+      # source_address_prefix                      = "*"
+      # source_application_security_group_ids      = ["*"]
+      # destination_address_prefixes               = ["*"]
+      # destination_application_security_group_ids = ["*"]
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet2" {
+  subnet_id                 = azurerm_subnet.subnet2.id
+  network_security_group_id = azurerm_network_security_group.subnet2_nsg.id
 }
--- modules/azurerm_virtual_network/variables.tf
+++ modules/azurerm_virtual_network/variables.tf
@@ -28,6 +28,22 @@ variable "subnet1_address_prefix" {
   type        = string
 }
 
+variable "subnet1_nsg_security_rules" {
+  description = "The security rules for the subnet 1."
+  type = list(object({
+    name                         = string
+    protocol                     = string
+    source_port_range            = string
+    destination_port_range       = string
+    source_address_prefixes      = list(string)
+    destination_address_prefixes = string
+    access                       = string
+    priority                     = number
+    direction                    = string
+  }))
+  default = null # Make this variable optional
+}
+
 variable "subnet2_name" {
   description = "The name of the second subnet."
   type        = string
@@ -36,6 +52,22 @@ variable "subnet2_name" {
 variable "subnet2_address_prefix" {
   description = "The address prefix for the second subnet."
   type        = string
+}
+
+variable "subnet2_nsg_security_rules" {
+  description = "The security rules for the subnet 2."
+  type = list(object({
+    name                         = string
+    protocol                     = string
+    source_port_range            = string
+    destination_port_range       = string
+    source_address_prefixes      = list(string)
+    destination_address_prefixes = string
+    access                       = string
+    priority                     = number
+    direction                    = string
+  }))
+  default = null # Make this variable optional
 }
 
 variable "tags" {
