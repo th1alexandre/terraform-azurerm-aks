675166e9a7efcaf74763dd2282385c8b557e9c91
th1alexandre <github@th1alexandre.dev>
2024-09-03 17:12:35 -0400

    new module: azure kubernetes service

---
+++ modules/azurerm_aks/main.tf
@@ -0,0 +1,59 @@
+resource "azurerm_kubernetes_cluster" "this" {
+  name                         = var.cluster_name
+  location                     = var.location
+  resource_group_name          = var.resource_group_name
+  dns_prefix                   = var.dns_prefix
+  automatic_channel_upgrade    = var.automatic_channel_upgrade
+  image_cleaner_enabled        = var.image_cleaner_enabled
+  image_cleaner_interval_hours = var.image_cleaner_interval_hours
+  kubernetes_version           = var.kubernetes_version
+  node_resource_group          = var.node_resource_group
+  private_cluster_enabled      = var.private_cluster_enabled
+  sku_tier                     = var.sku_tier
+
+  network_profile {
+    network_plugin = var.network_plugin
+    ip_versions    = var.ip_versions
+    service_cidrs  = var.service_cidrs
+    dns_service_ip = var.dns_service_ip
+  }
+
+  linux_profile {
+    admin_username = var.admin_username
+
+    ssh_key {
+      key_data = var.ssh_public_key
+    }
+  }
+
+  api_server_access_profile {
+    authorized_ip_ranges = var.authorized_ip_ranges
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  default_node_pool {
+    name           = var.system_node_pool_name
+    vm_size        = var.system_vm_size
+    node_count     = var.system_node_count
+    vnet_subnet_id = var.system_vnet_subnet_id
+
+    enable_host_encryption       = var.system_enable_host_encryption
+    only_critical_addons_enabled = var.system_only_critical_addons_enabled
+    temporary_name_for_rotation  = var.system_temporary_name_for_rotation
+
+    upgrade_settings {
+      drain_timeout_in_minutes      = 0
+      max_surge                     = "10%"
+      node_soak_duration_in_minutes = 0
+    }
+
+    node_labels = var.system_node_labels
+
+    tags = var.tags
+  }
+
+  tags = var.tags
+}
---
+++ modules/azurerm_aks/variables.tf
@@ -0,0 +1,139 @@
+# Cluster variables
+variable "cluster_name" {
+  description = "The name of the AKS cluster"
+  type        = string
+}
+
+variable "location" {
+  description = "The region of the AKS cluster"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group for the AKS cluster"
+  type        = string
+}
+
+variable "dns_prefix" {
+  description = "The DNS prefix specified for the AKS cluster"
+  type        = string
+}
+
+variable "automatic_channel_upgrade" {
+  description = "The upgrade channel selected for the AKS cluster"
+  type        = string
+}
+
+variable "image_cleaner_enabled" {
+  description = "Enable the image cleaner for the AKS cluster"
+  type        = bool
+}
+
+variable "image_cleaner_interval_hours" {
+  description = "The interval in hours to run the image cleaner"
+  type        = number
+}
+
+variable "kubernetes_version" {
+  description = "The version to use when creating the AKS cluster"
+  type        = string
+}
+
+variable "node_resource_group" {
+  description = "Name of the new resource group for the node resources"
+  type        = string
+}
+
+variable "private_cluster_enabled" {
+  description = "Expose the API server only on internal IP addresses"
+  type        = bool
+}
+
+variable "sku_tier" {
+  description = "The SKU tier for the AKS cluster"
+  type        = string
+}
+
+# Network Profile
+variable "network_plugin" {
+  description = "The network plugin to use for the AKS cluster"
+  type        = string
+}
+
+variable "ip_versions" {
+  description = "IPv4 or dual stack IPv4/IPv6"
+  type        = list(string)
+}
+
+variable "service_cidrs" {
+  description = "CIDR range for services, IPv4 or dual stack IPv4/IPv6"
+  type        = list(string)
+}
+
+variable "dns_service_ip" {
+  description = "IPv4 address within the Kubernetes service address range that will be used by cluster service discovery (kube-dns)"
+  type        = string
+}
+
+# Linux Profile
+variable "admin_username" {
+  description = "The username for the Linux Virtual Machines"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "The SSH public key"
+  type        = string
+}
+
+# API Server Access Profile
+variable "authorized_ip_ranges" {
+  description = "IP ranges authorized to access the API server"
+  type        = list(string)
+}
+
+# System Node Pool (Default)
+variable "system_node_pool_name" {
+  description = "The name of the system node pool"
+  type        = string
+}
+
+variable "system_vm_size" {
+  description = "The size of the Virtual Machines in the node pool"
+  type        = string
+}
+
+variable "system_node_count" {
+  description = "The number of nodes in the node pool"
+  type        = number
+}
+
+variable "system_vnet_subnet_id" {
+  description = "The ID of the subnet in which to deploy the node pool"
+  type        = string
+}
+
+variable "system_enable_host_encryption" {
+  description = "Enable host encryption"
+  type        = bool
+}
+
+variable "system_only_critical_addons_enabled" {
+  description = "Enable only critical addons"
+  type        = bool
+}
+
+variable "system_temporary_name_for_rotation" {
+  description = "Temporary name for rotation"
+  type        = string
+}
+
+variable "system_node_labels" {
+  description = "A mapping of labels to assign to the system node pool"
+  type        = map(string)
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the cluster resources"
+  type        = map(string)
+}
