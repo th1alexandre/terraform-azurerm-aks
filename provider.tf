5bdcbaf7f5a03a1ed92b0651f62de2234dc770b7
th1alexandre <github@th1alexandre.dev>
2024-09-03 17:20:39 -0400

    init local provider

--- provider.tf
+++ provider.tf
@@ -4,9 +4,16 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.116.0"
     }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.1"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
 }
+
+provider "local" {}
