459e898db7d5bf9022f9d200276464b1de7f4833
th1alexandre <github@th1alexandre.dev>
2024-09-03 17:20:04 -0400

    create aks cluster

--- main.tf
+++ main.tf
@@ -30,3 +30,44 @@ module "virtual_network" {
 
   tags = var.tags
 }
+
+module "kubernetes_cluster" {
+  source = "./modules/azurerm_aks"
+
+  cluster_name                 = var.aks_cluster_name
+  location                     = local.region_location
+  resource_group_name          = local.resource_group
+  dns_prefix                   = var.aks_dns_prefix
+  automatic_channel_upgrade    = var.aks_automatic_channel_upgrade
+  image_cleaner_enabled        = var.aks_image_cleaner_enabled
+  image_cleaner_interval_hours = var.aks_image_cleaner_interval_hours
+  kubernetes_version           = var.aks_kubernetes_version
+  node_resource_group          = var.aks_node_resource_group
+  private_cluster_enabled      = var.aks_private_cluster_enabled
+  sku_tier                     = var.aks_sku_tier
+
+  # Network Profile
+  network_plugin = var.aks_network_plugin
+  ip_versions    = var.aks_ip_versions
+  service_cidrs  = var.aks_service_cidrs
+  dns_service_ip = var.aks_dns_service_ip
+
+  # Linux Profile
+  admin_username = var.aks_admin_username
+  ssh_public_key = file(".ssh/id_rsa.pub")
+
+  # API Server Access Profile
+  authorized_ip_ranges = var.aks_authorized_ip_ranges
+
+  ## System Node Pool (Default)
+  system_node_pool_name               = var.aks_system_node_pool_name
+  system_vm_size                      = var.aks_system_vm_size
+  system_node_count                   = var.aks_system_node_count
+  system_vnet_subnet_id               = module.virtual_network.subnet1_id
+  system_node_labels                  = var.aks_system_node_labels
+  system_enable_host_encryption       = var.aks_system_enable_host_encryption
+  system_only_critical_addons_enabled = var.aks_system_only_critical_addons_enabled
+  system_temporary_name_for_rotation  = var.aks_system_temporary_name_for_rotation
+
+  tags = var.tags
+}
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-# Resource Group
+## Resource Group
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
@@ -9,7 +9,13 @@ variable "resource_group_location" {
   type        = string
 }
 
-# Virtual Network
+## All resources
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+}
+
+## Virtual Network
 variable "vnet_name" {
   description = "The name of the virtual network"
   type        = string
@@ -40,8 +46,117 @@ variable "subnet2_address_prefix" {
   type        = string
 }
 
-# All resources
-variable "tags" {
-  description = "A map of tags to add to all resources"
+## AKS Cluster
+variable "aks_cluster_name" {
+  description = "The name of the AKS cluster"
+  type        = string
+}
+
+variable "aks_dns_prefix" {
+  description = "The DNS prefix specified for the AKS cluster"
+  type        = string
+}
+
+variable "aks_automatic_channel_upgrade" {
+  description = "The upgrade channel selected for the AKS cluster"
+  type        = string
+}
+
+variable "aks_image_cleaner_enabled" {
+  description = "Enable the image cleaner for the AKS cluster"
+  type        = bool
+}
+
+variable "aks_image_cleaner_interval_hours" {
+  description = "The interval in hours to run the image cleaner"
+  type        = number
+}
+
+variable "aks_kubernetes_version" {
+  description = "The version to use when creating the AKS cluster"
+  type        = string
+}
+
+variable "aks_node_resource_group" {
+  description = "Name of the new resource group for the node resources"
+  type        = string
+}
+
+variable "aks_private_cluster_enabled" {
+  description = "Expose the API server only on internal IP addresses"
+  type        = bool
+}
+
+variable "aks_sku_tier" {
+  description = "The SKU tier for the AKS cluster"
+  type        = string
+}
+
+# Network Profile
+variable "aks_network_plugin" {
+  description = "The network plugin to use for the AKS cluster"
+  type        = string
+}
+
+variable "aks_ip_versions" {
+  description = "IPv4 or dual stack IPv4/IPv6"
+  type        = list(string)
+}
+
+variable "aks_service_cidrs" {
+  description = "CIDR range for services, IPv4 or dual stack IPv4/IPv6"
+  type        = list(string)
+}
+
+variable "aks_dns_service_ip" {
+  description = "IPv4 address within the Kubernetes service address range that will be used by cluster service discovery (kube-dns)"
+  type        = string
+}
+
+# Linux Profile
+variable "aks_admin_username" {
+  description = "The username for the Linux Virtual Machines"
+  type        = string
+}
+
+# API Server Access Profile
+variable "aks_authorized_ip_ranges" {
+  description = "IP ranges authorized to access the API server"
+  type        = list(string)
+}
+
+# System Node Pool (Default)
+variable "aks_system_node_pool_name" {
+  description = "The name of the system node pool"
+  type        = string
+}
+
+variable "aks_system_vm_size" {
+  description = "The size of the Virtual Machines in the node pool"
+  type        = string
+}
+
+variable "aks_system_node_count" {
+  description = "The number of nodes in the node pool"
+  type        = number
+}
+
+variable "aks_system_enable_host_encryption" {
+  description = "Enable host encryption"
+  type        = bool
+}
+
+variable "aks_system_only_critical_addons_enabled" {
+  description = "Enable only critical addons"
+  type        = bool
+}
+
+variable "aks_system_temporary_name_for_rotation" {
+  description = "Temporary name for rotation"
+  type        = string
+}
+
+variable "aks_system_node_labels" {
+  description = "A mapping of labels to assign to the system node pool"
   type        = map(string)
 }
