8783123dd7b380d733531763c74787607014a9cd
th1alexandre <github@th1alexandre.dev>
2024-09-05 19:52:10 -0400

    add comments to improve readability

--- backend-init.sh
+++ backend-init.sh
@@ -3,16 +3,30 @@
 set -e
 
 init_local_backend() {
-  sed -i "1,8s/^/# /" backend.tf
+  # Comment the backend configuration block
+  sed -i "22,29s/^/# /" backend.tf
+
+  # Migrate to local backend
   terraform init -force-copy
+
+  # Change the prevent_destroy value to false, allowing the backend resources to be destroyed
   sed -i "s/\btrue\b/false/g" modules/backend/main.tf
+
+  # Destroy the remote backend resources
   terraform destroy -target=module.backend -auto-approve
 }
 
 init_remote_backend() {
+  # Create the required backend resources
   terraform apply -target=module.backend -auto-approve
+
+  # Change the prevent_destroy value to true, preventing the backend resources from being destroyed
   sed -i "s/\bfalse\b/true/g" modules/backend/main.tf
-  sed -i "1,8s/^# //" backend.tf
+
+  # Uncomment the azurerm backend configuration
+  sed -i "22,29s/^# //" backend.tf
+
+  # Migrate the local backend to the remote backend
   terraform init -force-copy
 }
 
