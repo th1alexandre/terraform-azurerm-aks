3c2703bd982cc6311da09b6dc92d1124da9b6c9b
th1alexandre <github@th1alexandre.dev>
2024-09-05 19:56:03 -0400

    require confirmation

--- backend-init.sh
+++ backend-init.sh
@@ -2,12 +2,27 @@
 
 set -e
 
+confirm() {
+  local prompt_message=$1
+  local cancelled_message=$2
+  read -p "$prompt_message Type 'yes' to proceed: " choice
+  if [ "$choice" != "yes" ]; then
+    echo "$cancelled_message"
+    exit 0
+  fi
+}
+
 init_local_backend() {
   # Comment the backend configuration block
   sed -i "22,29s/^/# /" backend.tf
 
   # Migrate to local backend
   terraform init -force-copy
+
+  # Confirm the destroy operation
+  CONFIRM_TF_DESTROY="Continuing will destroy the remote backend resources. PROCEED WITH CAUTION!! Do you want to continue?"
+  DESTROY_CANCELLED="Remote backend resources were not destroyed. Exiting..."
+  confirm "$CONFIRM_TF_DESTROY" "$DESTROY_CANCELLED"
 
   # Change the prevent_destroy value to false, allowing the backend resources to be destroyed
   sed -i "s/\btrue\b/false/g" modules/backend/main.tf
@@ -31,9 +46,21 @@ init_remote_backend() {
 }
 
 if [ "$1" = "-backend=local" ]; then
+  # Confirm the operation
+  LOCAL_INIT_CONFIRMATION="Proceeding will migrate the tfstate to local and destroy the remote backend resources. Do you want to continue?"
+  LOCAL_INIT_CANCELLED="Local backend was not initialized. Exiting..."
+  confirm "$LOCAL_INIT_CONFIRMATION" "$LOCAL_INIT_CANCELLED"
+
+  # Initialize the local backend
   init_local_backend
   exit 0
 elif [ "$1" = "-backend=azurerm" ]; then
+  # Confirm the operation
+  REMOTE_INIT_CONFIRMATION="Proceeding will create the remote backend resources and migrate the tfstate to remote. Do you want to continue?"
+  REMOTE_INIT_CANCELLED="Remote backend was not initialized. Exiting..."
+  confirm "$REMOTE_INIT_CONFIRMATION" "$REMOTE_INIT_CANCELLED"
+
+  # Initialize the remote backend
   init_remote_backend
   exit 0
 else
