6b19304923cdcd763278c332e2e15797adb1fb85
th1alexandre <github@th1alexandre.dev>
2024-09-07 02:10:10 -0400

    update cert-manager module

--- modules/helm/cert-manager/main.tf
+++ modules/helm/cert-manager/main.tf
@@ -5,15 +5,21 @@ module "cert-manager-namespace" {
 }
 
 resource "helm_release" "this" {
-  chart      = "cert-manager"
-  name       = "cert-manager"
-  namespace  = "cert-manager"
-  repository = "https://charts.jetstack.io"
-  version    = var.chart_version
+  chart           = "cert-manager"
+  name            = "cert-manager"
+  namespace       = "cert-manager"
+  repository      = "https://charts.jetstack.io"
+  version         = var.chart_version
+  cleanup_on_fail = true
 
   set {
     name  = "crds.enabled"
     value = true
+  }
+
+  set {
+    name  = "crds.keep"
+    value = false
   }
 
   depends_on = [module.cert-manager-namespace]
