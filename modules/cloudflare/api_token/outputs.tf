e23b3d6023eeb47156ecf7139eeb3398d0786786
th1alexandre <github@th1alexandre.dev>
2024-09-13 01:32:29 -0400

    cloudflare module to create api token

---
+++ modules/cloudflare/api_token/outputs.tf
@@ -0,0 +1,4 @@
+output "token" {
+  value = cloudflare_api_token.this.value
+  sensitive = true
+}
---
+++ modules/cloudflare/api_token/variables.tf
@@ -0,0 +1,9 @@
+variable "token_name" {
+  description = "The name of the Cloudflare API token"
+  type        = string
+}
+
+variable "zone_id" {
+  description = "The ID of the Cloudflare zone"
+  type        = string
+}
