96738b804c5fff7cfdab2d423b116f5a63dacc84
th1alexandre <github@th1alexandre.dev>
2024-09-13 15:39:10 -0400

    require token with zone read access

--- modules/cloudflare/zone/variables.tf
+++ modules/cloudflare/zone/variables.tf
@@ -2,3 +2,8 @@ variable "zone_name" {
   description = "The name of the Cloudflare zone"
   type        = string
 }
+
+variable "zone_read_token" {
+  description = "The Cloudflare API token with read access to the zone"
+  type        = string
+}
